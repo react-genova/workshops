f83a722bf4f8f067f63b7d332eec944d715254b7
ayleesedai <andrea.briozzo@gmail.com>
2018-05-29 09:12:43 +0200

    Created scaffhold for workshop 4

--- 03/src/mouse-wrapper-example/src/index.js
+++ 03/src/mouse-wrapper-example/src/index.js
@@ -4,6 +4,15 @@ import Both from "./children/Both";
 import * as Styles from "./index.style.js";
 import Application from './exercises/KeyLogger';
 
+let test = null;
+if(process.env.NODE_ENV === 'production') {
+    test = 'produzione';
+}
+else {
+    test = 'sviluppo';
+}
+export { test };
+
 const App = () => (
   <div style={Styles.main}>
     <Application />
